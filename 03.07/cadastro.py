50ad9c1c11a05f86413b97f9e7b2292e836b9c7f
Rodrigo Rebouças <rodrigor@dcx.ufpb.br>
2016-03-07 11:21:47 -0300

    Lista e for

---
+++ 03.07/cadastro.py
@@ -0,0 +1,9 @@
+nomes = []
+
+nomes.append("Joao")
+nomes.append("Maria")
+nomes.append("Jose")
+nomes.append("Filismina")
+
+for nome in nomes:
+    print(nome)
---
+++ 03.07/media.py
@@ -0,0 +1,9 @@
+notas = [10,8,5,4,2,10,9,8]
+
+soma = 0
+for nota in notas:
+    soma = soma + nota
+
+media = soma / len(notas)
+print(soma)
+print("A media é :",media)
